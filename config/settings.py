3d0617af48ab96c110e69545a63c85927c406508
pawelszopa <expiren@gmail.com>
2021-03-19 13:01:58 +0100

    refinement

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8&95nol%-zp0!hk_ida9=@-2#!uj&t*5j%s5%05^(db!e*2esi'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -137,13 +137,10 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
-        # 'rest_framework.authentication.BasicAuthentication',   jest niebezpieczny
         'rest_framework.authentication.TokenAuthentication',
 
     ]
 }
-# Normally AllowAny nut can be exchange with anything from posts.views
-
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
